54f9a386667f05fef991ccc2bfabbd5e412f7dee
forforeach <forforeach@gmail.com>
2020-05-01 21:58:22 +0300

    refactor(core): use array mrthods instead of loops

--- src/base-servuex.js
+++ src/base-servuex.js
@@ -10,32 +10,36 @@ export class BaseServuex {
   }
 
   initialize() {
-    const methodNames = this.getAllMethodNames()
-    this.decorateMethods(methodNames)
+    const methods = this.getAllMethodNames()
+    this.decorateMethods(methods)
   }
 
   getAllMethodNames() {
-    let methodNames = new Set()
+    let methods = {}
     let proto = this
     while (proto && proto.constructor !== BaseServuex) {
-      const currentMethodNames = Object.getOwnPropertyNames(proto).filter(
-        (property) => typeof proto[property] === 'function' && property !== 'constructor'
-      )
-      methodNames = new Set([...methodNames, ...currentMethodNames])
+      const descriptors = Object.getOwnPropertyDescriptors(proto)
+      const currentMethods = Object.entries(descriptors)
+        .filter(([name, descriptor]) => typeof descriptor.value === 'function' && name !== 'constructor')
+        .reduce((acc, [name, descriptor]) => {
+          acc[name] = descriptor
+          return acc
+        }, {})
+      methods = { ...currentMethods, ...methods }
       proto = Object.getPrototypeOf(proto)
     }
-    return methodNames
+    return methods
   }
 
-  decorateMethods(methodNames) {
-    for (const methodName of methodNames) {
-      if (!this[methodName].decorated) {
-        const originalMethod = this[methodName]
-        this[methodName] = async function decorator(...params) {
+  decorateMethods(methods) {
+    Object.entries(methods).forEach(([name, descriptor]) => {
+      if (!this[name].decorated) {
+        const originalMethod = descriptor.value
+        this[name] = async function decorator(...params) {
           return originalMethod.apply(this, params)
         }.bind(this)
-        this[methodName].decorated = true
+        this[name].decorated = true
       }
-    }
+    })
   }
 }
