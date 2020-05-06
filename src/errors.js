817efedf18032099486bb969e327c6dba6f66889
forforeach <forforeach@gmail.com>
2020-05-06 18:08:49 +0300

    refactor(*): remove unused condition and update test script

--- src/errors.js
+++ src/errors.js
@@ -5,10 +5,6 @@ class BaseServuexError extends Error {
     super(`Servuex: ${message}`)
     const constructor = Object.getPrototypeOf(this).constructor
     this.name = constructor.name
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, constructor)
-    }
   }
 }
 
